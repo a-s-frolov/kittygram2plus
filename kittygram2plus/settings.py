4e11420a2aa8f25787bef411ba438e01187f8360
Андрей Фролов <asfrolov@yandex.ru>
2022-04-11 22:06:20 +0300

    во дела

--- kittygram2plus/settings.py
+++ kittygram2plus/settings.py
@@ -4,11 +4,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-i8n#^u!c+u95k0b2*uraj)8b00'
 '(%p3ip9f*ze7s&+%8$r4bi5m'
 
