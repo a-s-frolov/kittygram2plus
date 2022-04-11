958e843657de8b39d22c16a21e26c6c7be36ac88
Андрей Фролов <asfrolov@yandex.ru>
2022-04-11 22:12:16 +0300

    sdvsdvsd

--- kittygram2plus/settings.py
+++ kittygram2plus/settings.py
@@ -60,9 +60,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'kittygram2plus.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
